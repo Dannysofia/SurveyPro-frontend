2f2850be229a2fd2bbced1dbdb01aee43e273e91
Dannysofia <danny.manquillo00@usc.edu.co>
2025-10-01 14:34:13 -0500

    Frontend proyecto SurveyPro

---
+++ src/views/VistaUser.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Esta es mi vista Usuarios</h1>
+    <HelloWorldVue class="color" :mensajes="mensajes" />
+  </template>
+  
+  <script setup>
+  import HelloWorldVue from "@/components/HelloWorld.vue";
+  
+  const mensajes = [
+    "Hola, soy la vista usuarios",
+    "Bienvenido al sistema",
+    "Este es un mensaje desde el componente padre"
+  ];
+  </script>
+  <style scoped>
+  .color{
+    background-color: aqua;
+  }
+  </style>
+  
